cdea278aa3cdee55d6494b0a47707c797a858082
Vinícius <vinijosenog@hotmail.com>
2018-06-16 03:50:29 -0300

    Atualizado imports do login

--- src/view/Login.java
+++ src/view/Login.java
@@ -1,24 +1,20 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import controller.ControlleLogin;
-
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.DropMode;
-import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import controller.ControlleLogin;
 
 public class Login extends JFrame implements ActionListener {
 
